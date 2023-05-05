da91f020c17dae58e29777358c9896ec38f90f55
Lennart Lopin <novalis78@gmail.com>
2023-05-05 17:34:56 -0400

    fixes

--- launch.sh
+++ launch.sh
@@ -1,8 +1,8 @@
 export INSIGHT_NETWORK=livenet 
 export BITCOIND_HOST="127.0.0.1"  
 export BITCOIND_USER=marscoinrpc 
-export BITCOIND_PASS=65VqB2sComWaHzMPPM5BR5wUNu2jnkouSvgQgSFawQgf 
+export BITCOIND_PASS=4YfHZeVW4bsXdavua6DZBv1kYjw6opeUtR2xV7UoeSek 
 export INSIGHT_PUBLIC_PATH=public 
 export BITCOIND_P2P_PORT=8338
 export INSIGHT_FORCE_RPC_SYNC=1
-nohup /home/node-v0.10.25-linux-x64/bin/node  ./node_modules/Marssight-api/insight.js &
+node ./node_modules/marssight-api/insight.js 
